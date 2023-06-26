80687474501f8cec65c14cf68293210a643a5f71
hiten36 <hitenkhatri14@gmail.com>
2023-06-27 02:21:00 +0530

    changes

--- index.js
+++ index.js
@@ -57,6 +57,29 @@ app.post("/contact", async (req, res) => {
         `,
   });
 
+  let resp1=await fetch(`https://accounts.zoho.in/oauth/v2/token?grant_type=refresh_token&client_id=1000.FS0PE9O76Z2VG1XDJFGG49O4J77ZKF&client_secret=e49d2b9e743e403ebba076fd28a05a80f6e5815833&refresh_token=1000.7cabfb8e30f390c31275783a09f4b907.2aea28f36e7defada84b8e4dc38ce432`,{
+    method:'POST'
+  });
+  let data1=await resp1.json();
+  console.log(data1);
+  let resp2=await fetch("https://www.zohoapis.in/crm/v4/Contacts",{
+    method:'POST',
+    headers:{
+      Authorization: `Bearer ${data1.access_token}`
+    },
+    body:JSON.stringify({  "data":[
+      {
+        "Department": service,
+        "First_Name": name,
+        "Last_Name": "-",
+        "Email": email,
+        "Description": message,
+        "Phone" : phone
+      }
+    ]})
+  });
+  let data2=await resp2.json();
+
   res.json({ success: true, message: "Email Sent Successfully" });
 });
 
@@ -151,8 +174,68 @@ app.post("/contact2", async (req, res) => {
         `,
   });
 
+  let resp1=await fetch(`https://accounts.zoho.in/oauth/v2/token?grant_type=refresh_token&client_id=1000.FS0PE9O76Z2VG1XDJFGG49O4J77ZKF&client_secret=e49d2b9e743e403ebba076fd28a05a80f6e5815833&refresh_token=1000.7cabfb8e30f390c31275783a09f4b907.2aea28f36e7defada84b8e4dc38ce432`,{
+    method:'POST'
+  });
+  let data1=await resp1.json();
+  console.log(data1);
+  let resp2=await fetch("https://www.zohoapis.in/crm/v4/Contacts",{
+    method:'POST',
+    headers:{
+      Authorization: `Bearer ${data1.access_token}`
+    },
+    body:JSON.stringify({  "data":[
+      {
+        "First_Name": name2,
+        "Last_Name": "-",
+        "Email": email2,
+        "Description": message2,
+        "Phone" : phone2
+      }
+    ]})
+  });
+  let data2=await resp2.json();
+
   res.json({ success: true, message: "Email Sent successfully" });
 });
+
+// {
+//   "access_token": "1000.3657f6fbd42dc38b5f06fbf321c9ecc8.bd3584c9403947deea1c1e68aab87f23",
+//   "refresh_token": "1000.bf95506572bf39c5e65f0193282c49ae.27cc0b5446a600eec1e53b05b8c804e2",
+//   "api_domain": "https://www.zohoapis.in",
+//   "token_type": "Bearer",
+//   "expires_in": 3600
+// }
+app.post('/test', async(req,res)=>{
+  try {
+      const { name, email, phone, service, message } = req.body;
+      let resp1=await fetch(`https://accounts.zoho.in/oauth/v2/token?grant_type=refresh_token&client_id=1000.FS0PE9O76Z2VG1XDJFGG49O4J77ZKF&client_secret=e49d2b9e743e403ebba076fd28a05a80f6e5815833&refresh_token=1000.7cabfb8e30f390c31275783a09f4b907.2aea28f36e7defada84b8e4dc38ce432`,{
+        method:'POST'
+      });
+      let data1=await resp1.json();
+      console.log(data1);
+      let resp2=await fetch("https://www.zohoapis.in/crm/v4/Contacts",{
+        method:'POST',
+        headers:{
+          Authorization: `Bearer ${data1.access_token}`
+        },
+        body:JSON.stringify({  "data":[
+          {
+            "Department": service,
+            "First_Name": name,
+            "Last_Name": "-",
+            "Email": email,
+            "Description": message,
+            "Phone" : phone
+          }
+        ]})
+      });
+      let data2=await resp2.json();
+      res.json({success: true, data: data2, message: 'Form submitted successfully'});
+  } catch (error) {
+    console.log(error.message);
+  }
+})
 
 app.listen(PORT, () => {
   console.log("server is runing on port", PORT);
